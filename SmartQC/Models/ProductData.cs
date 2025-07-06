fce0aadaa2e848d51662f2fab54eec2ff7ac4c9c
CHK404 <azbxabcd4@gmail.com>
2025-07-06 20:53:37 +0900

    feat: itemlistviewmodel

--- SmartQC/Models/ProductData.cs
+++ SmartQC/Models/ProductData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,47 @@ namespace SmartQC.Models
 {
     public class ProductData
     {
-        [Key]
-        public string? ProductName { get; set; }
-        public int Quantity {  get; set; }
-        public int Defective {  get; set; }
+        private string _productName = "";
+        public string ProductName
+        {
+            get => _productName;
+            set
+            {
+                if (_productName != value)
+                {
+                    _productName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private int _defective;
+        public int Defective
+        {
+            get => _defective;
+            set
+            {
+                if (_defective != value)
+                {
+                    _defective = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
     }
 }
