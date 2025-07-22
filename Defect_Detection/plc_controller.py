1da6a62ad8e675e63fea266b1caa764882d7f481
CHK404 <azbxabcd4@gmail.com>
2025-07-22 13:26:40 +0900

    Feat) DB_Connection & Name Changes

--- Defect_Detection/plc_controller.py
+++ Defect_Detection/plc_controller.py
@@ -4,21 +4,25 @@ import time
 from pymodbus.client.sync import ModbusTcpClient
 
 model = YOLO("C:/Users/서민재/runs/detect/train7/weights/best.pt")
-#plc연동 로직
-client = ModbusTcpClient('192.168.0.10', port=502)#이건 현수한테 물어보고 숫자만 바꾸면 됨
+
+# plc연동 로직
+client = ModbusTcpClient('192.168.0.10', port=502)
 client.connect()
-#이부분도 메모리 주소 plc로직 배선한 릴레이 보고 현수랑 맞춰서
+
 ADDR_STOP = 10     # %MX10
 ADDR_START = 11    # %MX11
 ADDR_OK = 5        # %MX5
 ADDR_NG = 6        # %MX6
+
 def trigger(address):
     client.write_coil(address, True, unit=1)
     time.sleep(0.3)
     client.write_coil(address, False, unit=1)
 cap = cv2.VideoCapture(0)
-#펄스 신호 중복 보내기 방지
+
+# 펄스 신호 중복 보내기 방지
 object_detected = False
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -28,12 +32,14 @@ while True:
     center_tolerance = 50  # 중앙 판별 범위
     results = model.predict(frame, conf=0.3)
     annotated_frame = results[0].plot()
+    
     if len(results[0].boxes) == 0:
         object_detected = False  # 박스 없으면 초기화
         cv2.imshow("Detect", annotated_frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
         continue
+    
     for box in results[0].boxes:
         x1, y1, x2, y2 = box.xyxy[0].tolist()
         cls_id = int(box.cls.item())
@@ -45,9 +51,11 @@ while True:
             abs(bbox_cx - cx_center) < center_tolerance and
             abs(bbox_cy - cy_center) < center_tolerance
         )
+        
         if not in_center:
             continue  # 중앙 아니면 무시
-        # ==== 원래 코드: 화면 표시 및 로그 출력 ====
+        
+        # 화면 표시 및 로그 출력
         if name == "cap_ng_dent":
             label = f"불량 - 구부러짐 (conf: {conf:.2f}) :x:"
             color = (0, 0, 255)
@@ -62,10 +70,12 @@ while True:
             print(":흰색_확인_표시: 병뚜껑 정상 (conf:", round(conf, 2), ")")
         else:
             continue
+        
         cv2.rectangle(annotated_frame, (int(x1), int(y1)), (int(x2), int(x2)), color, 2)
         cv2.putText(annotated_frame, label, (int(x1), int(y1) - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
-        #여기부터 추가된 PLC 연동 로직(신호 보내기)
+        
+        # 신호 보내기
         if not object_detected:
             trigger(ADDR_STOP)     # 컨베이어 정지
             time.sleep(1.0)        # 안정화 대기
@@ -75,10 +85,12 @@ while True:
                 trigger(ADDR_NG)
             trigger(ADDR_START)    # 컨베이어 재시작
             object_detected = True  # 중복 방지
+            
     # ==== 마지막 화면 출력 ====
     cv2.imshow("Detect", annotated_frame)
     if cv2.waitKey(1) & 0xFF == 27:  # ESC로 종료
         break
+    
 cap.release()
 cv2.destroyAllWindows()
 client.close()
